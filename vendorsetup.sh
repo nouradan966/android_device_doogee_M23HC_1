9eef1f65b85c06b684142dcfa6feaf7c541134b1
nouradan966 <149276956+nouradan966@users.noreply.github.com>
2025-08-31 16:39:06 -0400

    Update vendorsetup.sh
    Changed Omni with twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_M23HC_1-user
-add_lunch_combo omni_M23HC_1-userdebug
-add_lunch_combo omni_M23HC_1-eng
+add_lunch_combo twrp_M23HC_1-user
+add_lunch_combo twrp_M23HC_1-userdebug
+add_lunch_combo twrp_M23HC_1-eng
