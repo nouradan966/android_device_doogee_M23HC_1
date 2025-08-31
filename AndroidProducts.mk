768db12468cfdc80d9e040e92d818d1670c079c5
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-08-31 21:43:48 +0200

    M23HC_1: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: doogee Model: V Max Pro Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_M23HC_1.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_M23HC_1-user \
+    omni_M23HC_1-userdebug \
+    omni_M23HC_1-eng
---
+++ omni_M23HC_1.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from M23HC_1 device
+$(call inherit-product, device/doogee/M23HC_1/device.mk)
+
+PRODUCT_DEVICE := M23HC_1
+PRODUCT_NAME := omni_M23HC_1
+PRODUCT_BRAND := DOOGEE
+PRODUCT_MODEL := V Max Pro
+PRODUCT_MANUFACTURER := doogee
+
+PRODUCT_GMS_CLIENTID_BASE := android-doogee
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_k6877v1_64_k419-user 12 SP1A.210812.016 1rck61v164bspP14 release-keys"
+
+BUILD_FINGERPRINT := DOOGEE/M23HC_1_EEA/M23HC_1:12/SP1A.210812.016/1730430676:user/release-keys
