b00722d0e8688725d75fad078c79542fbdf3cdc9
nouradan966 <149276956+nouradan966@users.noreply.github.com>
2025-08-31 16:43:34 -0400

    Update and rename omni_M23HC_1.mk to twrp_M23HC_1.mk
    Changed Omni with twrp

--- twrp_M23HC_1.mk
+++ twrp_M23HC_1.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from M23HC_1 device
 $(call inherit-product, device/doogee/M23HC_1/device.mk)
 
 PRODUCT_DEVICE := M23HC_1
-PRODUCT_NAME := omni_M23HC_1
+PRODUCT_NAME := twrp_M23HC_1
 PRODUCT_BRAND := DOOGEE
 PRODUCT_MODEL := V Max Pro
 PRODUCT_MANUFACTURER := doogee
